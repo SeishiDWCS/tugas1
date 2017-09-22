c2e6144708d2dc717b5ba8a384b925fc6b2ce40d
SeishiDWCS <31976350+SeishiDWCS@users.noreply.github.com>
2017-09-22 09:23:25 +0700

    Add files via upload
    Tugas 1 Selesai

---
+++ tugas1_04217011.py
@@ -0,0 +1,12 @@
+Nama = "Nama : DanisWara Cahya Satria"
+Nim = "Nim  : 04217011"
+Umur = 17
+a = (Umur + 5)
+
+
+print (Nama)
+print (Nim)
+print "umur :"
+print (Umur)
+print "umur setelah lima tahun :"
+print (a)
