2c0dab8023a36f092a341e64a3e6833237b8de79
SeishiDWCS <31976350+SeishiDWCS@users.noreply.github.com>
2017-09-27 09:04:12 +0700

    Add files via upload

---
+++ Tugas1_04217011.py
@@ -0,0 +1,11 @@
+nama = "DanisWara Cahya Satria"
+nim = "04217011"
+umur = 17
+a = umur + 5
+
+print "Nama : " + nama
+print "Nim  : " + nim
+print "Umur : "
+print umur
+print "Umur setelah 5 tahun : "
+print a
