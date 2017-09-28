f263c7c2c9df24139e2786e1d2cd60e7412e732f
SeishiDWCS <31976350+SeishiDWCS@users.noreply.github.com>
2017-09-29 00:27:13 +0700

    Update Tugas1_04217011.py

--- Tugas1_04217011.py
+++ Tugas1_04217011.py
@@ -3,9 +3,9 @@ nim = "04217011"
 umur = 17
 a = umur + 5
 
-print "Nama : " + nama
-print "Nim  : " + nim
-print "Umur : "
-print umur
-print "Umur setelah 5 tahun : "
-print a
+print ("Nama : " + nama)
+print ("Nim  : " + nim)
+print ("Umur : ")
+print (umur)
+print ("Umur setelah 5 tahun : ")
+print (a)
